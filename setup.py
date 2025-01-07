c05a4461ee5189edd5ad05822fec15dc78adeea0
zhenxiay <157483470+zhenxiay@users.noreply.github.com>
2025-01-07 17:56:27 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='StockIntelligence',
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=[
+        'pandas',
+        'yfinance',
+        'google',
+        'lxml'
+    ],
+    author="zhenxiay",
+    author_email="yu.zhenxiao.yz@gmail.com",
+    url="https://github.com/zhenxiay/StockIntelligence"
+)
