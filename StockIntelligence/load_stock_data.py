62f94f0c807c3cd6ec2c4a6c3c3d90f8b59038e1
zhenxiay <157483470+zhenxiay@users.noreply.github.com>
2025-01-08 20:18:03 +0100

    Update load_stock_data.py

--- StockIntelligence/load_stock_data.py
+++ StockIntelligence/load_stock_data.py
@@ -1,1 +1,19 @@
-import yfinance as yf
+
+class LoadStockData(GetStockData):
+    def __init__(self, name, load_period, project, dataset):
+        super().__init__(name, load_period)
+        self.project = project
+        self.dataset = dataset
+
+    @staticmethod
+    def create_big_query_client():
+        client = bigquery.Client()
+        job_config = bigquery.LoadJobConfig(write_disposition="WRITE_TRUNCATE")
+        return client, job_config
+
+    def load_stock_data_to_big_query(self,table_name):
+        dataset = GetStockData(self.name, self.load_period)
+        table_id = f'{self.project}.{self.dataset}.{table_name}'
+        client, job_config = self.create_big_query_client()
+        client.load_table_from_dataframe(dataset, table_id, job_config=job_config)
+        print(f'Data load to big query {table_id} successfully!')
