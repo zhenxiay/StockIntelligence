8effc064e03c67e204fcded07c6774efd0d4150c
zhenxiay <157483470+zhenxiay@users.noreply.github.com>
2025-01-10 15:27:29 +0100

    Update load_stock_data.py
    Add method for incremental (append last day data) load to big query

--- StockIntelligence/load_stock_data.py
+++ StockIntelligence/load_stock_data.py
@@ -8,14 +8,29 @@ class LoadStockData(GetStockData):
         self.dataset = dataset
 
     @staticmethod
-    def create_big_query_client():
+    def create_big_query_client_full_load():
         client = bigquery.Client()
         job_config = bigquery.LoadJobConfig(write_disposition="WRITE_TRUNCATE")
+        return client, job_config
+
+    @staticmethod
+    def create_big_query_client_append():
+        client = bigquery.Client()
+        job_config = bigquery.LoadJobConfig(write_disposition="WRITE_APPEND")
         return client, job_config
 
     def load_stock_data_to_big_query(self,table_name):
         dataset = GetStockData(self.name, self.load_period).read_daily_data()
         table_id = f'{self.project}.{self.dataset}.{table_name}'
-        client, job_config = self.create_big_query_client()
+        client, job_config = self.create_big_query_client_full_load()
+        client.load_table_from_dataframe(dataset, table_id, job_config=job_config)
+        print(f'Data load to big query {table_id} successfully!')
+
+    def load_stock_data_to_big_query_incremental(self,table_name):
+        # fetch the last row of data frame for append
+        dataset = GetStockData(self.name, self.load_period).read_daily_data().tail(1)
+        table_id = f'{self.project}.{self.dataset}.{table_name}'
+        # use job config for append data load in big query
+        client, job_config = self.create_big_query_client_append()
         client.load_table_from_dataframe(dataset, table_id, job_config=job_config)
         print(f'Data load to big query {table_id} successfully!')
