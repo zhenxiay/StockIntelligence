b729b8ee75dc411311058ad384a9b61df28b9515
zhenxiay <157483470+zhenxiay@users.noreply.github.com>
2025-02-27 12:18:37 +0100

    Update load_stock_data.py

--- StockIntelligence/load_stock_data.py
+++ StockIntelligence/load_stock_data.py
@@ -1,6 +1,6 @@
 from google.cloud import bigquery
 from StockIntelligence.get_stock_data import GetStockData
-from StockIntelligence.db_utility import big_query_setup
+from StockIntelligence.db_utility.big_query_setup import create_big_query_client_full_load, create_big_query_client_append
 
 class LoadStockData():
     def __init__(self, name, load_period, project, dataset):
@@ -12,7 +12,7 @@ class LoadStockData():
     def load_stock_data_to_big_query(self,table_name):
         dataset = GetStockData(self.name, self.load_period).read_daily_data()
         table_id = f'{self.project}.{self.dataset}.{table_name}'
-        client, job_config = big_query_setup.create_big_query_client_full_load()
+        client, job_config = create_big_query_client_full_load()
         client.load_table_from_dataframe(dataset, table_id, job_config=job_config)
         print(f'Data load to big query {table_id} successfully!')
 
@@ -21,6 +21,6 @@ class LoadStockData():
         dataset = GetStockData(self.name, self.load_period).read_daily_data().tail(1)
         table_id = f'{self.project}.{self.dataset}.{table_name}'
         # use job config for append data load in big query
-        client, job_config = big_query_setup.create_big_query_client_append()
+        client, job_config = create_big_query_client_append()
         client.load_table_from_dataframe(dataset, table_id, job_config=job_config)
         print(f'Data load to big query {table_id} successfully! Number of loaded rows: {len(dataset)}...')
