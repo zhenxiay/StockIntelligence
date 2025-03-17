af29af807f3e252c282cd301f45e2b801eb1f379
zhenxiay <157483470+zhenxiay@users.noreply.github.com>
2025-03-17 15:02:32 +0100

    Update test_multi_data_load.py

--- tests/test_multi_data_load.py
+++ tests/test_multi_data_load.py
@@ -1,12 +1,14 @@
 from StockIntelligence.get_stock_data import GetStockData
-from StockIntelligence.load__multi_stock_data import LoadMultiStockData
-
-def test_read_multi_stock_data():
-    dataset = GetStockData('MSFT', '3mo')
-    columns_count = len(dataset.read_daily_data().columns)
-    assert columns_count == 15
+from StockIntelligence.load_multi_stock_data import LoadMultiStockData
 
 #Test that the an object can be created from the class load stock data
-def test_load_stock_data():
-    dataset = LoadStockData('MSFT',"3mo","bq_dataset_dummy","StockIntelligence")
+def test_load_multi_stock_data():
+    stock_list = ['MSFT','ASML']
+    
+    load_object = LoadMultiStockData(stock_list,
+                                 "3mo",
+                                 "bq_dataset_dummy",
+                                 "StockIntelligence")
+
+    dataset = load_object.reate_combinded_dataset(self, stock_list)
     assert dataset.name == 'MSFT'
