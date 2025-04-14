90208bc5f08755c0e2bb6653cc54230f4bf0771e
zhenxiay <157483470+zhenxiay@users.noreply.github.com>
2025-04-14 11:02:31 +0200

    Add new tests to check data ingestion into a sqlite3 db

--- tests/test_multi_data_load.py
+++ tests/test_multi_data_load.py
@@ -1,15 +1,20 @@
 from StockIntelligence.get_stock_data import GetStockData
 from StockIntelligence.load_multi_stock_data import LoadMultiStockData
 
-#Test that the an object can be created from the class load stock data
-def test_load_multi_stock_data():
-    stock_list = ['MSFT','ASML']
-    
-    load_object = LoadMultiStockData(stock_list,
+stock_list = ['MSFT','ASML']
+load_object = LoadMultiStockData(stock_list,
                                  "3mo",
                                  "bq_dataset_dummy",
                                  "StockIntelligence")
 
-    dataset = load_object.create_combined_dataset(stock_list)
+#Test that the an object can be created from the class load stock data
+def test_read_multi_stock_data():
+
+    dataset = load_object.create_combined_dataset()
     distinct_count = dataset['Ticker'].nunique()
     assert distinct_count == len(stock_list)
+
+def test_load_multi_stock_data():
+    load_object.load_multi_stock_data_to_sqlite3(db_path= '.',
+                                                 db_name= 'stock_db',
+                                                 table_name= 'test')
