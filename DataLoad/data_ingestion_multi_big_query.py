c4c735692c4374f13a26f68c8224635441fd9073
zhenxiay <157483470+zhenxiay@users.noreply.github.com>
2025-04-15 11:16:19 +0200

    Update data_ingestion_multi_big_query.py

--- DataLoad/data_ingestion_multi_big_query.py
+++ DataLoad/data_ingestion_multi_big_query.py
@@ -1,1 +1,10 @@
+from StockIntelligence.load_multi_stock_data import LoadMultiStockData
 
+stock_list = ['MSFT','NVDA','SNOW','TEAM','ASML']
+
+load_object = LoadMultiStockData(stock_list, 
+                                 "2y", 
+                                 "keen-vial-420113", 
+                                 "StockIntelligence")
+
+load_object.load_multi_stock_data_to_big_query('portfolio_analysis')
