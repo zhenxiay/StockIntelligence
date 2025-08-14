7581325608566ed0c8301c7ee3c72a18fb229fbd
Yu Zhenxiao (MA/BDO72) <Zhenxiao.Yu@de.bosch.com>
2025-08-14 15:48:05 +0200

    Clean up: Remove empty __init__.py files and enhance docstrings for clarity in stock data handling modules

--- StockIntelligence/technical_analysis.py
+++ StockIntelligence/technical_analysis.py
@@ -10,12 +10,12 @@ def calc_rsi(df_input, rsi_window):
     This function calculates the Relative Strength Index (RSI) for given stock data.
 
     Args:
-    
+
     df_input (DataFrame): Input DataFrame containing stock data with a 'Close' column.
     rsi_window (list): List of integers representing the RSI calculation windows.
     '''
     for window in rsi_window:
-        df_input[f'rsi_{window}'] = rsi(close= X["Close"], 
-                                 window= window, 
+        df_input[f'rsi_{window}'] = rsi(close= X["Close"],
+                                 window= window,
                                  fillna= False)
-    return X
+    return df_input
