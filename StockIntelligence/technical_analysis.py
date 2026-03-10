0eb444e212d2eacffc1a8651520baa4a96f38402
Yu Zhenxiao (MA/BDO72) <Zhenxiao.Yu@de.bosch.com>
2026-03-10 11:37:28 +0100

    Add new technical indicator williams r to load stock data frame

--- StockIntelligence/technical_analysis.py
+++ StockIntelligence/technical_analysis.py
@@ -3,7 +3,7 @@ This module contains functions for calculating technical indicators using the `t
 It includes functions to calculate the Relative Strength Index (RSI) for given stock data.
 '''
 
-from ta.momentum import rsi
+from ta.momentum import rsi, williams_r
 
 def calc_rsi(df_input, rsi_window):
     '''
@@ -15,7 +15,34 @@ def calc_rsi(df_input, rsi_window):
     rsi_window (list): List of integers representing the RSI calculation windows.
     '''
     for window in rsi_window:
-        df_input[f'rsi_{window}'] = rsi(close= df_input["Close"],
-                                 window= window,
-                                 fillna= False)
+        rsi_series = rsi(
+            close= df_input["Close"],
+            window= window,
+            fillna= False
+        )
+
+        df_input = df_input.assign(**{f'rsi_{window}': rsi_series})
+
+    return df_input
+
+def calc_williams_r(df_input):
+    '''
+    Calculate Williams %R for given stock data.
+
+    Args:
+
+    df_input (DataFrame): Input DataFrame containing stock data with a 'Close' column.
+    '''
+
+    williams_r_series = williams_r(
+        high=df_input["High"],
+        low=df_input["Low"],
+        close=df_input["Close"],
+        fillna=False
+    )
+
+    df_input = df_input.assign(
+        williams_r=williams_r_series
+        )
+
     return df_input
